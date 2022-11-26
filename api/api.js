098d4e476b6f7825000597360f206b9a000a92ed
Kingbaks <89915105+Kingbaks@users.noreply.github.com>
2022-11-26 18:08:47 +0100

    fin justering av koden

--- api/api.js
+++ api/api.js
@@ -1,11 +1,9 @@
-const url   = 'http://gik2f8-labs.herokuapp.com/books';
-
+const url = "http://gik2f8-labs.herokuapp.com/books";
 
 async function getAll() {
-    const res = await fetch(url);
-    const json = await res.json();
-    return json;
-    /* return fetch(url).then((result) => result.json())
+	const res = await fetch(url);
+	const json = await res.json();
+	return json;
+	/* return fetch(url).then((result) => result.json())
     .then((jsonData) => jsonData); */
-    
 }
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-'use strict';
+"use strict";
 
 let bookList = [];
-   
-window.addEventListener('load',() => {
-    getAll().then((apiBooks) => (bookList = apiBooks));
 
-
+window.addEventListener("load", () => {
+	getAll().then((apiBooks) => (bookList = apiBooks));
 });
 // const searchInput = document.children[0].children[1].children[1];
 // const searchField = document.getElementById('searchField');
@@ -15,64 +13,53 @@ window.addEventListener('load',() => {
 
 searchField.addEventListener("keyup", handleKeyPress);
 
-function handleKeyPress (e) {
-    /*
+function handleKeyPress(e) {
+	/*
     Ta emot/läsa av värdet i input
     skick värdet till searchbook
     searchBooks returnera en filtrerad lista
     filtrerade listan skickas till renderlist 
     */
-    searchBooks(e.target.value);
+	searchBooks(e.target.value);
 }
 
-// another way to use the searchfiled 
+// another way to use the searchfiled
 /* searchField.addEventListener("keyup", (e) =>
 renderBookList(
     bookList.filter(({author, title}) =>  {
         const searchTerm = e.target.value.toLowerCase();
         return    author.toLowerCase().indexOf(searchTerm.toLowerCase())>= 0 ||   title.toLowerCase().indexOf(searchTerm)>= 0 ); */
 
-
 async function searchBooks(searchTerm) {
-     //const bookList = await getAll()
-     const filteredList = bookList.filter(({author, title}) =>  author.toLowerCase().indexOf(searchTerm.toLowerCase())>= 0 || 
-      title.toLowerCase().indexOf(searchTerm.toLowerCase())>= 0 );
+	//const bookList = await getAll()
+	const filteredList = bookList.filter(
+		({ author, title }) =>
+			author.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ||
+			title.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0
+	);
 
-  /*  for ( let i = 0; i < bookList.length; i++) {
+	/*  for ( let i = 0; i < bookList.length; i++) {
     //const title = bookList[i].title.toLowerCase();
     const author = bookList[i].author.toLowerCase();
     
     if (author.indexOf(searchTerm.toLowerCase()) >= 0 ) {
         filteredList.push(bookList[i]);
     } */
-     renderBookList(filteredList);
-   
-  
+	renderBookList(filteredList);
 }
 
-function renderBookList (bookList){
+function renderBookList(bookList) {
+	const existingElement = document.querySelector(".book-list");
+	console.log(existingElement);
 
+	const root = document.getElementById("root");
+	if (existingElement) {
+		root.removeChild(existingElement);
+	}
 
-
-  const existingElement = document.querySelector(".book-list");
-  console.log(existingElement);
-  
-  const root = document.getElementById('root');
-  if (existingElement) {
-    root.removeChild(existingElement);
-  }
-
-
-if (bookList.length > 0 && searchField.value) {
-
-  root.insertAdjacentHTML('beforeend', BookList(bookList)); 
-
-}
+	if (bookList.length > 0 && searchField.value) {
+		root.insertAdjacentHTML("beforeend", BookList(bookList));
+	}
 }
 
 /* getAll().then((bookList) => console.log(bookList)); */
-
-
-
-
-
